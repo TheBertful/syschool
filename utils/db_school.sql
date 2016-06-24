dc00ba06b4200a96a6be012e5d7fc1f06155fb10
TheBertful <robertosmoure@gmail.com>
2016-06-24 19:02:37 -0300

    Script do banco zoado

--- utils/db_school.sql
+++ utils/db_school.sql
@@ -79,10 +79,7 @@ CREATE TABLE `disciplina` (
   `id_disciplina` int(8) NOT NULL AUTO_INCREMENT,
   `nome_disciplina` varchar(50) DEFAULT NULL,
   `id_curso` int(8),
-<<<<<<< HEAD
-=======
-  `horario` varchar(5), 
->>>>>>> origin/master
+  `horario` varchar(5),
   PRIMARY KEY (`id_disciplina`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
